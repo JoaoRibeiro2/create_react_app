64033c77c1c29c7a22d218a49322be87fdb4397f
JoaoRibeiro2 <joaoribeyro1@gmail.com>
2020-09-24 18:09:59 +0000

    main class

--- ts/main.ts
+++ ts/main.ts
@@ -1,11 +1,13 @@
 namespace empresa{
     
-    let p = new Pessoa();
-    p.nome = "Maria";
-    p.idade = 20;
+    let c = new Cliente()
+    c.nome = "Pedro"
+    c.idade = 30;
+    c.codCliente = 123;
 
+    document.querySelector("#nome").textContent = c.nome
+    document.querySelector("#idade").textContent = c.idade.toString()
+    document.querySelector("#anoNasc").textContent = c.anoNasc(2020).toString()
 
-    document.querySelector("#nome").textContent = p.nome
-    document.querySelector("#idade").textContent = p.idade.toString()
-    document.querySelector("#anoNasc").textContent = p.anoNasc(2020).toString()
+    
 }
