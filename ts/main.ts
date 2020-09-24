5caad4335309c1963ccaf56ba8f7a825b80569af
JoaoRibeiro2 <joaoribeyro1@gmail.com>
2020-09-24 22:53:38 +0000

    principal class

--- ts/main.ts
+++ ts/main.ts
@@ -4,10 +4,11 @@ namespace empresa{
     c.nome = "Pedro"
     c.idade = 30;
     c.codCliente = 123;
+    c.credito = 100
 
     document.querySelector("#nome").textContent = c.nome
     document.querySelector("#idade").textContent = c.idade.toString()
     document.querySelector("#anoNasc").textContent = c.anoNasc(2020).toString()
-
-    
+    document.querySelector("#codCliente").textContent = c.codCliente.toString()
+    document.querySelector("#credito").textContent = c.credito.toString()
 }
